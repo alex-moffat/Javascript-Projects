78efac983c520490f28b2c1d4ce118a7ef2bc3ed
Alex Moffat <wamoffat4@gmail.com>
2020-05-14 13:42:47 -0700

    Step 64
    - Write function with += operator
    - Create an HTML button element to call and display function
    + Style button and paragraph elements with function
    + Add logo to title tab

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_2.js
@@ -1,70 +1,63 @@
 //========== VARIABLES ==========
-var A = "This is a string."
-var My_Alert = "This is my string alert with my age 49"
-var My_Age = 49
-var Quote = "This is my string alert where I say \"I cant believe I am 49\"."
-var S = "- William Shakespeare"
-var C = "combine 1" + " with 2"
+var My_color = "#472F70"
+var My_Alert = "Welcome to my website."
+var AM = " - Alex Moffat"
+My_Alert += AM
+
 var Family = "Moffat", Dad="Alex", Mom="Lisa", Son="Zan", Daughter="Grace", Dog="Albert";
-var My_color = "purple"
 Family= Family.fontcolor("lightseagreen"), Dad= Dad.fontcolor("#6A5ACD"), Mom= Mom.fontcolor("rgb(100,149,237)"), Son= Son.fontcolor("darkblue"), Daughter= Daughter.fontcolor("#EE82EE"), Dog= Dog.fontcolor("burlywood");
-var E_Value = (10 * 10)
+
 
 
 //========== FUNCTIONS & METHODS ==========
 
-window.alert("Hello World!");
-document.write("Hello World!");
-
-document.write(A);
-
 window.alert(My_Alert);
-
-document.write(Quote);
-
-document.write(" \"To be, or not to be? "
-    + "That is the question.\" "
-    + S);
-
-document.write(C);
-
-document.write("The " + Family + " family has a dog named " + Dog + ".");
-
-document.write(Family + ", " + Dad + ", " + Mom + ", " + Son + ", " + Daughter + ", and " + Dog);
-
-document.write(E_Value);
 
 //========== EVENT FUNCTIONS ==========
 
 function clickMe() {
     var B = "I love the color purple!"; // assign variable for button text
     var P = "Paragraph changed to tomato!"; // assign variable for paragraph text
+   
     // change text color and bold button text
     B = B.fontcolor("#634697"); 
     B = B.bold(); 
+    
     // change color and font size for paragraph
     P = P.fontcolor("tomato");
     P = P.fontsize(16); 
     document.getElementById("Purple_Text").innerHTML = B; //return colored text to button
-    document.getElementById("Tomato_Text").innerHTML = P; //reutrn colored text to paragraph
+    document.getElementById("Tomato_Text").innerHTML = P; //return colored text to paragraph    
 }
 
 function hoverOver() { // change text inside button on mouse over
     var T = "I DARE YOU";
     document.getElementById("Dare_Button").innerHTML = T;
+    document.getElementById("Dare_Button").style.fontSize = "2rem";
+    document.getElementById("Dare_Button").style.padding = "2%";
+    document.getElementById("Dare_Button").style.backgroundColor = "red";
+    document.getElementById("Dare_Button").style.color = "white";
 }
 
 function hoverAway() { // change text back on button when mouse moves away 
     var T = "Click Me";
     document.getElementById("Dare_Button").innerHTML = T;
+    document.getElementById("Dare_Button").style.fontSize = "initial";
+    document.getElementById("Dare_Button").style.padding = "initial";
+    document.getElementById("Dare_Button").style.backgroundColor = "initial";
+    document.getElementById("Dare_Button").style.color = "initial";
 }
 
 function boom() { // Add paragraph text and set style properties: center text and fontsize increase
+    var A = document.getElementById("Aftermath").innerHTML; // get previous string value from text element
+    A += " Now you've done it and you can't take it back."; // concatenate the previous string 
     var T = "BOOM!";
     T = T.fontcolor("red");
     document.getElementById("Boom_Text").innerHTML = T;
     document.getElementById("Boom_Text").style.textAlign = "center";
     document.getElementById("Boom_Text").style.fontSize = "12rem";
+    document.getElementById("Aftermath").innerHTML = A; //return amended text paragraph
+    document.getElementById("Aftermath").style.textDecoration = "underline overline"; //style amended text
 }
 
 function unBoom() { // remove paragraph text and reset style properties
